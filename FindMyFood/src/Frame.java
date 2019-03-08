d071308621ec22e4ffdc2c20e65834b9ea05ca86
arajiv <arajiv@calpoly.edu>
2019-03-07 16:01:51 -0800

    made the frame

---
+++ FindMyFood/src/Frame.java
@@ -0,0 +1,14 @@
+import java.awt.BorderLayout;
+
+import javax.swing.*; 
+public class Frame extends JFrame {
+	
+	JFrame frame; 
+	public Frame() { 
+		setTitle("Find My Food"); 
+		setSize(400,500); 
+		setLayout(null); 
+		setVisible(true); 
+	}
+
+}
